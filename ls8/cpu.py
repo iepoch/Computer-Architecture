af06731f780d0b2fb85e987853b89d50e8956f06
Stefan <steaclem@gmail.com>
2019-08-24 14:44:37 -0700

    Fixed the Load program into memory

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,6 +4,7 @@ import sys
 HLT = 0b00000001
 PRN = 0b01000111
 LDI = 0b10000010
+MUL = 0b10100010
 
 
 class CPU:
@@ -17,12 +18,13 @@ class CPU:
         self.pc = 0
         # where we can store values in the reg
         self.reg = [0] * 8
-        self.op_halt = False
-        self.op_LDI = 0b10000010
+        self.op_hlt = False
+
         self.inst = {
             HLT: self.op_halt,
             LDI: self.op_LDI,
-
+            MUL: self.op_mul,
+            PRN: self.op_PRN
         }
 
     def ram_read(self, address):
@@ -31,34 +33,36 @@ class CPU:
     def ram_write(self, value, address):
         self.ram[address] = value
 
-    def op_halt(self):
-        if HLT is True:
-            return True
+    def op_mul(self, operand_a, operand_b):
+        self.alu('MUL', operand_a, operand_b)
 
-    def opt_LDI(self):
-        if LDI == self.op_LDI:
-            return True
+    def op_halt(self, address, value):
+        self.op_hlt = True
 
-    def load(self):
+    def op_LDI(self, address, value):
+        self.reg[address] = value
+
+    def op_PRN(self, address, operand_b):
+        print(self.reg[address])
+
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        with open(filename) as file:
+            for line in file:
+                comment_split = line.split('#')
+                instruction = comment_split[0]
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+                if instruction == '':
+                    continue
+                # print(instruction)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                first_bit = instruction[0]
+                if first_bit == "0" or first_bit == "1":
+                    self.ram[address] = int(instruction[:8], 2)
+                    address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -66,6 +70,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -91,29 +97,39 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        """Some instructions requires up to the next two bytes 
+        """Some instructions requires up to the next two bytes
         of data after the PC in memory to perform operations on.
-        Sometimes the byte value is a register number, 
+        Sometimes the byte value is a register number,
         other times it's a constant value (in the case of LDI).
-        Using ram_read(), read the bytes at PC+1 and PC+2 
-        from RAM into variables operand_a and operand_b in case 
+        Using ram_read(), read the bytes at PC+1 and PC+2
+        from RAM into variables operand_a and operand_b in case
         the instruction needs them."""
 
         # running = True
 
-        while not self.op_halt:
+        while not self.op_hlt:
             # Storeing the result in IR from reading
             # the memory address that is stored in register PC
             IR = self.ram[self.pc]
+
+            # print("IR", IR)
             operand_a = self.ram_read(self.pc + 1)
             # print(operand_a)
             operand_b = self.ram_read(self.pc + 2)
-            print(operand_b)
-            if IR == HLT:
-                self.op_halt = True
-            elif IR == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif IR == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
+
+            op_size = IR >> 6
+            ins_set = ((IR >> 4) & 0b1) == 1
+
+            # if IR == HLT:
+            #     self.op_halt=True
+            # elif IR == LDI:
+            #     self.reg[operand_a]=operand_b
+            #     self.pc += 3
+            # elif IR == PRN:
+            #     print(self.reg[operand_a])
+            #     self.pc += 2
+            if IR in self.inst:
+                self.inst[IR](operand_a, operand_b)
+
+            if not ins_set:
+                self.pc += op_size + 1
